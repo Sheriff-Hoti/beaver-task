0a54267749a60faa8b3ee3863239324056f1de8a
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-11-29 14:55:34 +0100

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ToDo TUI APP written in Go";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs =
